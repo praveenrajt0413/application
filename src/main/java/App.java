37daaff9b78df92b333037a9a549eb4f34f6c513
praveenraj2023-jes <praveenmtechse2023@gmail.com>
2026-03-28 11:44:55 +0530

    Ver4.5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
 public class App {
-    public static int add(int a, int b) {
-        return a + b;
-    }
 
     public static void main(String[] args) {
-        System.out.println("Hello DevOps!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello DevOps on 8081!";
+    }
+
+    // for JUnit testing
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
